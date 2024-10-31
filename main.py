a304272da680952026ffa68f11ba0ebc3e878ea1
sufyaniitk <totallynotsufyan@gmail.com>
2024-10-31 18:09:25 +0530

    15% accuracy lesgo

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import torch
+import math
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -15,18 +16,33 @@ labels = data['targets'] # (2500,)
 def mahalanobis_distance(x, y):
     diff = x - y
     M = np.matmul(diff, diff.T)
-    return np.sqrt(M.trace)
+    return np.sqrt(np.trace(M))
+
+def greyscale(img):
+    """
+    img: x * y * 3
+    """
+    gs = [[0] * len(img[0]) for _ in range(len(img))]
+
+    for i in range(len(img)):
+        for j in range(len(img[0])):
+            gs[i][j] = 0.3 * img[i][j][0] + 0.59 * img[i][j][1] + 0.11 * img[i][j][2]
+    
+    res = np.asanyarray(gs)
+    return res
 
 def learn_prototypes(images, labels, num_classes):
     prototypes = np.zeros((num_classes, 32, 32))
     for i in range(num_classes):
         class_samples = [images[j] for j in range(2500) if labels[j] == i]
-     #    prototypes[i] = np.mean(class_samples, axis=0)
-     #    for idx, img in enumerate(class_samples):
-     #      for x in range(32):
-     #           for y in range(32):
-                    
-    prototypes[i] = (idx * prototypes[i] + img) / (idx + 1)
+        greyscales = []
+        for sample in class_samples:
+            sample_greyscale = greyscale(sample)
+            greyscales.append(sample_greyscale)
+        
+        greyscales = np.asanyarray(greyscales)
+        prototypes[i] = np.mean(class_samples)
+    
     return prototypes
 
 def compute_inverse_covariance(images):
@@ -37,16 +53,16 @@ def compute_inverse_covariance(images):
 def classify_with_prototypes(images, prototypes):
     predictions = []
     for img in images:
-        distances = [mahalanobis_distance(img, proto) for proto in prototypes]
+        transformed_img = greyscale(img)
+        distances = [mahalanobis_distance(transformed_img, proto) for proto in prototypes]
         predictions.append(np.argmin(distances))
     return np.array(predictions)
 
 def main():
     num_classes = 10
     prototypes = learn_prototypes(images, labels, num_classes)
-#     inv_cov = compute_inverse_covariance(images)
     predictions = classify_with_prototypes(images, prototypes)
-    accuracy = np.mean(predictions == labels.numpy())
+    accuracy = np.mean(predictions == np.asarray(labels))
     print(f'Accuracy: {accuracy * 100:.2f}%')
     visualize_predictions(images, predictions, labels)
 
@@ -54,7 +70,7 @@ def visualize_predictions(images, predictions, labels):
     plt.figure(figsize=(10, 10))
     for i in range(25):
         plt.subplot(5, 5, i + 1)
-        plt.imshow(images[i].reshape(32, 32), cmap='gray')
+        plt.imshow(greyscale(images[i]), cmap='gray')
         plt.title(f'Pred: {predictions[i]}, True: {labels[i]}')
         plt.axis('off')
     plt.show()
